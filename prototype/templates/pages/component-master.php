098c66224c50f281aa990fdaf30930ef80cdcc6b
Jon Pearse <jon@jonpearse.net>
2016-08-03 17:15:48 +0100

    Tidying up

--- prototype/templates/pages/component-master.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$this->layout('layouts::component');
-?>
-
-<main id="main-content" role="main" class="main-content">
-	<div class="band <?php if ( !empty($classnames) ): echo $classnames; endif; ?>">
-        <?php $this->insert('components::'.$component); ?>
-	</div>
-</main>
