9a92abdd8556bcfb5b5914321e2ee571cb47a938
Jon Pearse <jon@jonpearse.net>
2016-08-08 14:49:18 +0100

    Beginning to tweak testimonial wall

--- prototype/templates/partials/common/testimonial-wall.php
+++ prototype/templates/partials/common/testimonial-wall.php
@@ -2,8 +2,8 @@
     <h2 class="vh">What our residents say</h2>
 
     <div class="grid">
-        <div class="gc l1-3 grid grid--vertical-t">
-            <article class="testimonial-wall__testimonial grid gc t1-2">
+        <div class="gc xxl1-3 grid grid--vertical-xxl">
+            <article class="testimonial-wall__testimonial grid gc t2-3 xxl1-2">
                 <aside class="testimonial-wall__image gc t1-2 box box--red">
                     <?php $this->insert('component::bleed-image', [ 'image' => '/_dummy/testimonial-wall/bio-kelly-chan.png' ]); ?>
                 </aside>
@@ -14,7 +14,7 @@
                     </p>
                 </div>
             </article>
-            <article class="testimonial-wall__testimonial gc t1-2 grid">
+            <article class="testimonial-wall__testimonial gc t1-3 xxl1-2 grid">
                 <div class="testimonial-wall__content box box--ink box--less-padding">
                     <h3>Tom Carter<br>Manchester</h3>
                     <p>
@@ -25,11 +25,11 @@
             </article>
 
         </div>
-        <article class="testimonial-wall__testimonial gc l3-6 grid">
-            <aside class="testimonial-wall__image gc t3-5 box box--red">
+        <article class="testimonial-wall__testimonial gc xxl3-6 grid">
+            <aside class="testimonial-wall__image gc t1-2 xxl3-5 box box--red">
                 <?php $this->insert('component::bleed-image', [ 'image' => '/_dummy/testimonial-wall/bio-sara-crosby.png' ]); ?>
             </aside>
-            <div class="testimonial-wall__content gc t2-5 box box--ink box--less-padding">
+            <div class="testimonial-wall__content gc t1-2 xxl2-5 box box--ink box--less-padding">
                 <h3>Sara Crosby<br>London</h3>
 
                 <p>
@@ -38,11 +38,11 @@
                 </p>
             </div>
         </article>
-        <article class="testimonial-wall__testimonial gc l1-6 grid grid--vertical-t">
-            <aside class="testimonial-wall__image gc t1-2 box box--red">
+        <article class="testimonial-wall__testimonial gc xxl1-6 grid grid--vertical-xxl">
+            <aside class="testimonial-wall__image gc t1-2 xxl1-2 box box--red">
                 <?php $this->insert('component::bleed-image', [ 'image' => '/_dummy/testimonial-wall/bio-pete-williams.png' ]); ?>
             </aside>
-            <div class="testimonial-wall__content gc t1-2 box box--red-alt box--less-padding">
+            <div class="testimonial-wall__content gc t1-2 xxl1-2 box box--red-alt box--less-padding">
                 <h3>Pete Williams<br>Nottingham</h3>
 
                 <p>
@@ -51,3 +51,4 @@
             </div>
         </article>
     </div>
+</section>
