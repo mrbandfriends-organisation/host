db3176eb98e57a6706b52b98a2b9d2ea00094d13
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-08-26 09:00:27 +0100

    Checkerboard updated with links to subpages

--- prototype/templates/components/checkerboard.php
+++ prototype/templates/components/checkerboard.php
@@ -34,7 +34,7 @@
                 <h3>Featured home<br>Our latest or greatest</h3>
 
                 <p>
-                    <a href="#" class="btn btn--white btn--small">Show me featured homes</a>
+                    <a href="/building.php" class="btn btn--white btn--small">Show me featured homes</a>
                 </p>
             </div>
         </li>
