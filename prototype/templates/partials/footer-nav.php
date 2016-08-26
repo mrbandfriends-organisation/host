15cb4b3136f8665717e54e8af6553554e0bf834c
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-08-26 09:34:04 +0100

    Footer linked up

--- prototype/templates/partials/footer-nav.php
+++ prototype/templates/partials/footer-nav.php
@@ -30,20 +30,20 @@
             <ul class="nav-footer__sublist">
             <?php foreach ($aCity AS $sCity): ?>
                 <li class="nav-footer__item">
-                    <a href="#" class="nav-footer__link"><?=$sCity; ?></a>
+                    <a href="/city.php" class="nav-footer__link"><?=$sCity; ?></a>
                 </li>
             <?php endforeach; ?>
             </ul>
         </li>
         <li class="nav-footer__item nav-footer__section gc t1-2 m1-5 l1-6">
             <strong class="nav-footer__section-header">
-                <a href="#" class="nav-footer__link">About Us</a>
+                <a href="/about.php" class="nav-footer__link">About Us</a>
             </strong>
 
             <ul class="nav-footer__sublist">
             <?php for ($i = 0; $i < 4; $i++): ?>
                 <li class="nav-footer__item">
-                    <a href="#" class="nav-footer__link">Link here</a>
+                    <a href="/about.php" class="nav-footer__link">Link here</a>
                 </li>
             <?php endfor; ?>
             </ul>
