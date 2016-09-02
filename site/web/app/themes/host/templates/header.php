87d499f265a5e87b19376ad57880d1c33adb2044
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-09-02 17:20:12 +0100

    Work on the page header

--- site/web/app/themes/host/templates/header.php
+++ site/web/app/themes/host/templates/header.php
@@ -2,18 +2,29 @@
     use Roots\Sage\Utils;
 ?>
 
-<header class="banner">
-  <div class="container">
-    <?php 
-        echo Utils\ob_load_template_part('templates/partials/site-logo.php');
-    ?>
-    
-    <?php
-        $banner_nav_args = array(
-            'modifier' => 'banner'
-        );
+<header class="banner js-banner" role="banner">
+	<div class="banner__inner container">
+		<div class="banner__section banner__brand">
+            <a href="/" class="banner__home-link"><?php echo Utils\ob_load_template_part('templates/partials/shared/icon', [ 'icon' => 'site-logo' ]); ?></a>
+		</div>
 
-        echo Utils\ob_load_template_part('templates/menus/primary-nav.php', $banner_nav_args);
-    ?>
-    </div>
+		<div class="banner__section banner__nav">
+            <?php echo Utils\ob_load_template_part('templates/partials/utility-nav', [ 'modifier' => 'banner' ]); ?>
+
+            <?php
+                $banner_nav_args = array(
+                    'modifier' => 'banner'
+                );
+
+                echo Utils\ob_load_template_part('templates/menus/primary-nav', $banner_nav_args);
+            ?>
+
+            <button id="menu-button" type="button" class="offcanvas-toggle offcanvas-toggle--open tcon tcon-menu--xcross js-offcanvas-toggle" aria-label="toggle menu" aria-expanded="false" aria-controls="menu">
+                <span class="offcanvas-toggle__text"><span class="vh">Open</span> menu</span>
+                <span class="offcanvas-toggle__lines tcon-menu__lines" aria-hidden="true"></span>
+            </button>
+		</div>
+
+
+	</div>
 </header>
--- site/web/app/themes/host/templates/partials/site-logo.php
+++ site/web/app/themes/host/templates/partials/site-logo.php
@@ -7,11 +7,12 @@
 */
 
 ?>
-<a class="site-logo" href="<?php echo esc_url( home_url('/') ); ?>">
+
+<a class="site-logo banner__home-link" href="<?php echo esc_url( home_url('/') ); ?>">
 	<picture>
 		<!--[if IE 9]><video style="display: none;"><![endif]-->
 		<source type="image/svg+xml" srcset="<?php echo esc_attr( get_stylesheet_directory_uri() . '/assets/svg/standalone/output/site-logo.svg'); ?>">
 		<!--[if IE 9]></video><![endif]-->
-		<img src="<?php echo esc_attr(get_stylesheet_directory_uri() .'/assets/svg/standalone/output/png/logo.png'); ?>" alt="<?php echo esc_attr(bloginfo('name'));?>">
+		<img src="<?php echo esc_attr(get_stylesheet_directory_uri() .'/assets/svg/standalone/output/png/site-logo.png'); ?>" alt="<?php echo esc_attr(bloginfo('name'));?>">
 	</picture>
 </a>
