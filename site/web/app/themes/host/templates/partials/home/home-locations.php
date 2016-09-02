129c7db68149013255e3f7472ef2300e39f73e64
Rob Lowe <rob.lowe@mrbandfriends.co.uk>
2016-09-02 15:16:05 +0100

    Home page location listing partial

---
+++ site/web/app/themes/host/templates/partials/home/home-locations.php
@@ -0,0 +1,39 @@
+<?php
+  /**
+  * HOME LOCATIONS
+  **/
+    use Roots\Sage\Utils;
+?>
+
+<?php
+  $header_title_1 = get_field('header_title_1');
+  $header_title_2 = get_field('header_title_2');
+?>
+
+<h2>
+  <?php echo esc_html($header_title_1); ?><br />
+  <?php echo esc_html($header_title_2); ?>
+</h2>
+
+
+<ul style="overflow:hidden;">
+  <?php
+    $location_query = new WP_Query( array(
+      'post_type' => array( 'locations' )
+    ) );
+
+    while ( $location_query->have_posts() ) : $location_query->the_post();
+
+      $carousel_images = get_field('carousel_images');
+      $carousel_image = $carousel_images[0]['sizes']['medium'];
+  ?>
+
+    <li style="width:15%;border:solid 1px red;float:left;height:100px;">
+      <a href="<?php echo the_permalink(); ?>">
+        <?php echo the_title(); ?>
+        <img src="<?php echo esc_html($carousel_image); ?>" />
+      </a>
+    </li>
+
+    <?php endwhile; ?>
+</ul>
