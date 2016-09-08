3868b559b9a6839cb832ef6fc49b51ad6fc60271
Stuart J Nelson <stuart@northernbadger.co.uk>
2016-09-08 17:28:40 +0100

    Updated banner menu

---
+++ site/web/app/themes/host/templates/partials/utility-nav.php
@@ -0,0 +1,9 @@
+<?php
+    use Roots\Sage\Utils;
+?>
+
+<?php wp_nav_menu( array(
+    'menu'              => 'Header Utilities',
+    'container_class'   => 'menu-utilities menu-utilities--banner',
+    'menu_class'        => 'nav-utilities nav-utilities--banner'
+)); ?>
