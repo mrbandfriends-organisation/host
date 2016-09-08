194ce44666420739c79bc735001248606fb8ea5a
Stuart J Nelson <stuart@northernbadger.co.uk>
2016-09-08 12:30:36 +0100

    Have basic lay0out nof off canvas menu in

--- site/web/app/themes/host/templates/partials/primary-offcanvas.php
+++ site/web/app/themes/host/templates/partials/primary-offcanvas.php
@@ -3,10 +3,18 @@
 ?>
 <!-- <div class="primary-offcanvas offcanvas__offscreen-right" aria-hidden="true"> -->
 <div class="primary-offcanvas" aria-hidden="true">
-    <button type="button" class="offcanvas-toggle offcanvas-toggle--close tcon tcon-menu--xcross tcon-transform js-offcanvas-toggle" aria-label="toggle menu">
-        <span class="tcon-menu__lines" aria-hidden="true"></span>
-        <span class="tcon-visuallyhidden">Close menu</span>
-    </button>
+    <div class="flex">
+        <div class="primary-offcanvas__logo-container">
+            <a href="/" class="banner__home-link">
+                <?php echo Utils\ob_load_template_part('templates/partials/shared/icon', [ 'icon' => 'site-logo', 'classnames' => 'svg-icon--site-logo svg-icon--white' ]); ?>
+            </a>
+        </div>
+
+        <button type="button" class="offcanvas-toggle offcanvas-toggle--close tcon tcon-menu--xcross tcon-transform js-offcanvas-toggle" aria-label="toggle menu">
+            <span class="tcon-menu__lines" aria-hidden="true"></span>
+            <span class="tcon-visuallyhidden">Close menu</span>
+        </button>
+    </div>
 
     <div class="primary-offcanvas__section">
         <?php
@@ -15,5 +23,20 @@
             );
             echo Utils\ob_load_template_part('templates/menus/primary-nav.php', $offcanvas_nav_args);
         ?>
+    </div>
+
+    <div class="primary-offcanvas__section primary-offcanvas__social-icons">
+        <ul class="offcanvas-social-icons flex">
+            <li class="offcanvas-social-icons__item">
+                <a href="#" class="offcanvas-social-icons__link">
+                    <?php echo Utils\ob_load_template_part('templates/partials/shared/icon', [ 'icon' => 'facebook', 'classnames' => 'svg-icon--sky' ]); ?>
+                </a>
+            </li>
+            <li class="offcanvas-social-icons__item">
+                <a href="#" class="offcanvas-social-icons__link">
+                    <?php echo Utils\ob_load_template_part('templates/partials/shared/icon', [ 'icon' => 'twitter', 'classnames' => 'svg-icon--sky' ]); ?>
+                </a>
+            </li>
+        </ul>
     </div>
 </div>
