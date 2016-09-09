b2337fc981e44aad5d644f96785cc551dbefefd6
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-09-09 15:15:12 +0100

    Removing <main> on home template

--- site/web/app/themes/host/template-home.php
+++ site/web/app/themes/host/template-home.php
@@ -6,19 +6,16 @@
 ?>
 
 
-<main id="main-content" role="main" class="main-content">
 
-  <?php echo Utils\ob_load_template_part('templates/partials/home/home-locations'); ?>
-
-
-  <?php echo Utils\ob_load_template_part('templates/partials/home/home-reasons'); ?>
+<?php echo Utils\ob_load_template_part('templates/partials/home/home-locations'); ?>
 
 
-  <?php echo Utils\ob_load_template_part('templates/partials/shared/featured-building'); ?>
+<?php echo Utils\ob_load_template_part('templates/partials/home/home-reasons'); ?>
 
 
-  <?php echo Utils\ob_load_template_part('templates/partials/shared/awards'); ?>
+<?php echo Utils\ob_load_template_part('templates/partials/shared/featured-building'); ?>
 
-  <?php echo Utils\ob_load_template_part('templates/partials/home/home-investors'); ?>
 
-</main>
+<?php echo Utils\ob_load_template_part('templates/partials/shared/awards'); ?>
+
+<?php echo Utils\ob_load_template_part('templates/partials/home/home-investors'); ?>
