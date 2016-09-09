27968d09e601b35d38bc92efe47097caecae0d47
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-09 16:18:12 +0100

    Added basic stacked gallerys tlying

--- site/web/app/themes/host/templates/components/bleed-image.php
+++ site/web/app/themes/host/templates/components/bleed-image.php
@@ -1,6 +1,7 @@
 <?php
-    $sBg = (empty($image)) ? '' : ' style="background-image:url('.esc_attr($image).')"';
+    $sBg        = (empty($image)) ? '' : ' style="background-image:url('.esc_attr($image).')"';
+    $modifier   = ( !empty($modifier) ? $modifier : null );
 ?>
-<div class="box bleed-image"<?=$sBg; ?>>
+<div class="box bleed-image <?php echo esc_attr($modifier); ?>"<?=$sBg; ?>>
     <img src="<?=esc_attr($image); ?>" alt="" class="bleed-image__image">
 </div>
--- site/web/app/themes/host/templates/partials/shared/stacked-gallery.php
+++ site/web/app/themes/host/templates/partials/shared/stacked-gallery.php
@@ -1,23 +1,29 @@
 <?php
     use Roots\Sage\Utils;
 
-    $images     = ( !empty($images) ? $images : null );
+    $images         = ( !empty($images) ? $images : null );
+    $image_counter  = ( !empty($images) ? count($images) : null);
+    $modifier       = ( !empty($modifier) ? 'stacked-gallery__bleed-image--' . $modifier : 'stacked-gallery__bleed-image' );
 ?>
 
 <?php if ( !empty($images) ): ?>
     <div class="stacked-gallery-container grid grid--vertical-l">
 
+        <?php  ?>
         <?php foreach ($images as $image): ?>
             <div class="stacked-gallery gc t1-2 l1-2">
-                <a href="#" class="stacked-gallery__link flex">
+                <a href="<?php echo esc_url($image['image']) ?>" class="stacked-gallery__link flex js-popup-gallery-trigger">
                     <div class="stacked-gallery__inner flex">
-                        <strong class="stacked-gallery__label">See all 12 photos</strong>
+                        <strong class="stacked-gallery__label">See all <?php echo esc_html($image_counter); ?> photos</strong>
                         <?php echo Utils\ob_load_template_part('templates/partials/shared/icon', array(
                             'icon'          => 'twitter',
                             'classnames'    => 'stacked-gallery__icon svg-icon--white'
                         )); ?>
                     </div>
-                    <?php echo Utils\ob_load_template_part('templates/components/bleed-image', [ 'image' => $image['image']] ); ?>
+                    <?php echo Utils\ob_load_template_part('templates/components/bleed-image', [
+                        'image'     => $image['image'],
+                        'modifier'  => $modifier
+                    ] ); ?>
                 </a>
             </div>
         <?php endforeach; ?>
