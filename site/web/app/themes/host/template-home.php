6f1f8cd5f6b0a0f88f90a3380c155f8f9985b4f6
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-12 17:31:11 +0100

    CMS’d up homepage reasons partial

--- site/web/app/themes/host/template-home.php
+++ site/web/app/themes/host/template-home.php
@@ -9,7 +9,10 @@
 
 <?php echo Utils\ob_load_template_part('templates/partials/home/home-locations'); ?>
 
-<?php echo Utils\ob_load_template_part('templates/partials/home/home-reasons'); ?>
+<?php echo Utils\ob_load_template_part('templates/partials/home/home-reasons', array(
+    'snippet' => '/home/reason'
+)); ?>
+
 
 <?php echo Utils\ob_load_template_part('templates/partials/shared/featured-building', array(
     'snippet' => '/home/building'
