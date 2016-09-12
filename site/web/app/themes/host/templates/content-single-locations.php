3fa1d1e2898875d339230a2430de90247bc3d62c
Jon Pearse <jon@jonpearse.net>
2016-09-12 14:23:46 +0100

    Removing duplicated <MAIN> elemtn

--- site/web/app/themes/host/templates/content-single-locations.php
+++ site/web/app/themes/host/templates/content-single-locations.php
@@ -1,21 +1,17 @@
 <?php
     use Roots\Sage\Utils;
 
-    while (have_posts()) : the_post();
-?>
+    while (have_posts())
+    {
+        the_post();
 
-<main id="main-content" role="main" class="main-content">
-<?php
-    echo Utils\ob_load_template_part('templates/partials/shared/header-carousel.php');
+        echo Utils\ob_load_template_part('templates/partials/shared/header-carousel.php');
 
-    echo Utils\ob_load_template_part('templates/partials/location/location-intro.php');
+        echo Utils\ob_load_template_part('templates/partials/location/location-intro.php');
 
-    echo Utils\ob_load_template_part('templates/partials/location/location-buildings.php');
+        echo Utils\ob_load_template_part('templates/partials/location/location-buildings.php');
 
-    echo Utils\ob_load_template_part('templates/partials/location/location-attractions.php');
+        echo Utils\ob_load_template_part('templates/partials/location/location-attractions.php');
 
-    echo Utils\ob_load_template_part('templates/partials/shared/map');
-?>
-</main>
-
-<?php endwhile; ?>
+        echo Utils\ob_load_template_part('templates/partials/shared/map');
+    }
