b62ce8aba9eb7463ee39c5b3bf8274ada13f9e1c
Jon Pearse <jon@jonpearse.net>
2016-09-14 10:42:44 +0100

    Top-aligning images on testimonial wall

--- site/web/app/themes/host/templates/snippets/shared/testimonial-grid-3.php
+++ site/web/app/themes/host/templates/snippets/shared/testimonial-grid-3.php
@@ -4,7 +4,8 @@ use Roots\Sage\Utils;
 
 <aside class="testimonial-wall__image gc t1-2 xxl3-5 box box--red">
     <?php echo Utils\ob_load_template_part('templates/components/bleed-image', array(
-         'image' => $image
+         'image'    => $image,
+         'modifier' => 'bleed-image--top'
     )); ?>
 </aside>
 <div class="testimonial-wall__content gc t1-2 xxl2-5 box box--ink box--less-padding">
