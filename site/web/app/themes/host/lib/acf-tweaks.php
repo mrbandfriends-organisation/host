adf737f8e4c73e00bdb58149c7d358a5e610b993
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-09-14 17:19:53 +0100

    Added Google map API key to ACF settings for admin

--- site/web/app/themes/host/lib/acf-tweaks.php
+++ site/web/app/themes/host/lib/acf-tweaks.php
@@ -5,18 +5,26 @@ namespace Roots\Sage\ACFTweaks;
 use Roots\Sage\Setup;
 
 /**
- * HIDE ACF GUI 
+ * HIDE ACF GUI
  *
  * making use of ACF save-to-JSON functionality means it's a very bad
  * idea to make changes to Custom Fields via the GUI on a non-VC env.
- * As a result this function will hide the GUI on a non VC env. 
+ * As a result this function will hide the GUI on a non VC env.
  */
 function my_acf_hide_gui( $show ) {
-    
+
     if (WP_ENV !== "development") { // show on version controlled envs
       return false;
-    } 
-    
-    return true;    
+    }
+
+    return true;
 }
 add_filter('acf/settings/show_admin', __NAMESPACE__ . '\\my_acf_hide_gui');
+
+
+function acf_add_gmaps_key() {
+
+	acf_update_setting('google_api_key', GMAPS_API_KEY);
+}
+
+add_action('acf/init', __NAMESPACE__ . '\\acf_add_gmaps_key');
