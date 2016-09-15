ab9ec6141fb865fa5b4b5287f0ac9cf80fc67616
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-15 12:34:49 +0100

    Created generic page tempalte

--- site/web/app/themes/host/templates/components/hero.php
+++ site/web/app/themes/host/templates/components/hero.php
@@ -10,21 +10,22 @@ use Roots\Sage\Assets;
  ?>
 
 <?php
-    $color = (empty($color)) ? '' : " box--{$color}";
-?>
+    $modifier = ( !empty($modifier) ? "hero--" . $modifier : null );
+    $color    = ( empty($color)) ? '' : " box--{$color}";
 
-<?php
+    // Setting up hero image
     $post_thumbnail_ref = get_post_thumbnail_id( $post_id );
-
     $img_srcset    = Assets\get_responsive_image( $post_thumbnail_ref, array(
         'wpthumb' => 'width=1600&height=0&crop=1&resize=1',
-        'class' => 'js-rimgbg-target',
+        'class' => 'js-rimgbg-target ',
         'dimensions' => [ 1600, 1200, 768, 600, 320 ]
     ));
  ?>
 
-<section class="band hero box<?=$color; ?>">
-    <div class="container hero__inner">
-        <?php echo $img_srcset; ?>
-    </div>
-</section>
+<?php if ( !empty($post_thumbnail_ref) ): ?>
+    <section class="band hero <?=$modifier; ?> box<?=$color; ?>">
+        <div class="container hero__inner">
+            <?php echo $img_srcset; ?>
+        </div>
+    </section>
+<?php endif; ?>
