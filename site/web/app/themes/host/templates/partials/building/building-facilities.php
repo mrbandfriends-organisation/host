9d3327df68312dd400723b48a05acdad649a2ce6
Cliff Williams <cliff@hiddenpause.com>
2016-09-15 18:01:23 +0100

    Added gallery to facilities

--- site/web/app/themes/host/templates/partials/building/building-facilities.php
+++ site/web/app/themes/host/templates/partials/building/building-facilities.php
@@ -10,22 +10,15 @@
     $facilities_title = get_field('facilities_title');
     $facilities_overview = get_field('facilities_overview');
     $facilities_location = 'London';
-
- if( have_rows('facilities_photos') ):
-
-      while ( have_rows('facilities_photos') ) : the_row();
-
-          $photo = get_sub_field('photo');
-          $photo_url = $photo['url'];
-
-      endwhile;
-  endif;
+    $facilities_images = get_field('facilities_photos');
 ?>
 
 <?php
 $main_content = Utils\ob_load_template_part('templates/snippets/building/facilities-introduction', compact('facilities_title', 'facilities_overview', 'facilities_location'));
 
-$aside_content = Utils\ob_load_template_part('templates/components/bleed-image', [ 'image' => $photo_url ]);
+$aside_content = Utils\ob_load_template_part('templates/partials/shared/mini-carousel', array(
+  'carousel_images' => $facilities_images
+));
 ?>
 
 
