678decacf00dc757cdc18b16dd7b1db480543e05
Jon Pearse <jon@jonpearse.net>
2016-09-15 16:17:25 +0100

    Tweaking proportions of location news feed

--- site/web/app/themes/host/templates/partials/location/location-feed.php
+++ site/web/app/themes/host/templates/partials/location/location-feed.php
@@ -2,10 +2,10 @@
     use Roots\Sage\Utils;
 ?>
 
-<section class="band news-feed" data-equality>
+<section class="band news-feed" data-equality="from:medium">
     <h2 class="vh">Latest news</h2>
     <div class="grid news-feed__grid">
-        <div class="gc l1-5">
+        <div class="gc m1-3 xxl1-5">
             <article class="news-feed__article box box--ink box--less-padding" data-equality-pane="2">
                 <header class="news-feed__article__header">
                     <h3 class="news-feed__article__title plain">
@@ -19,15 +19,15 @@
                 </p>
             </article>
         </div>
-        <div class="gc l4-5 grid--vertical-l">
-            <div class="gc l1-2 grid">
-                <div class="gc l1-4">
+        <div class="gc m2-3 xxl4-5">
+            <div class="grid">
+                <div class="gc xxl1-4 gc--above-xxl">
                     <?=Utils\ob_load_template_part('templates/components/bleed-image', [
                         'image'    => '/app/uploads/2016/08/girl-alpha.png',
                         'modifier' => 'box--grape-dark'
                     ]); ?>
                 </div>
-                <div class="gc l1-2">
+                <div class="gc m1-2">
                     <article class="news-feed__article box box--ink box--less-padding" data-equality-pane>
                         <header class="news-feed__article__header">
                             <h3 class="news-feed__article__title plain">
@@ -41,27 +41,27 @@
                         </p>
                     </article>
                 </div>
-                <div class="gc l1-4">
+                <div class="gc m1-2 xxl1-4 gc--above-m">
                     <?=Utils\ob_load_template_part('templates/components/bleed-image', [
                         'image'    => '/app/uploads/2016/08/girl-alpha.png',
                         'modifier' => 'box--grape-dark'
                     ]); ?>
                 </div>
             </div>
-            <div class="gc l1-2 grid">
-                <div class="gc l1-4">
+            <div class="grid">
+                <div class="gc xxl1-4 gc--above-xxl">
                     <?=Utils\ob_load_template_part('templates/components/bleed-image', [
                         'image'    => '/app/uploads/2016/08/girl-alpha.png',
                         'modifier' => 'box--grape-dark'
                     ]); ?>
                 </div>
-                <div class="gc l1-4">
+                <div class="gc m1-2 xxl1-4 gc--above-m">
                     <?=Utils\ob_load_template_part('templates/components/bleed-image', [
                         'image'    => '/app/uploads/2016/08/girl-alpha.png',
                         'modifier' => 'box--grape-dark'
                     ]); ?>
                 </div>
-                <div class="gc l1-2">
+                <div class="gc m1-2">
                     <article class="news-feed__article box box--ink box--less-padding" data-equality-pane>
                         <header class="news-feed__article__header">
                             <h3 class="news-feed__article__title plain">
