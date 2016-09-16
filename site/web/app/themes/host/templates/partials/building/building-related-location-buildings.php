d08266b0f9ea8a329309bb3a963a74dc42984f02
Stuart J Nelson <stuart@northernbadger.co.uk>
2016-09-16 15:46:38 +0100

    Created connceted location to building. Then connected buildings to that location

--- site/web/app/themes/host/templates/partials/building/building-related-location-buildings.php
+++ site/web/app/themes/host/templates/partials/building/building-related-location-buildings.php
@@ -1,8 +1,14 @@
 <?php
-  /**
-  * BUILDING PEOPLE
-  **/
+    /**
+     * LOCATION BUILDINGS
+     **/
     use Roots\Sage\Utils;
-?>
+    use Roots\Sage\RoomsBuildings;
 
-<h1>I am realated</h1>
+    $id                            = get_the_id();
+    $connected_location            = host_building_find_connected_location( $id );
+    $connected_location_id         = $connected_location->post->ID;
+    $location_conncected_buildings = host_locations_find_connected_building($connected_location_id);
+
+    var_dump( $location_conncected_buildings );
+?>
