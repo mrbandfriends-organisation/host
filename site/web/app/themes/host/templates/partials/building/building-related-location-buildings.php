f4ea03e404629087daf70a882cda182033e885b3
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-19 10:00:23 +0100

    Installed slick slider. Added partial inside while loop

--- site/web/app/themes/host/templates/partials/building/building-related-location-buildings.php
+++ site/web/app/themes/host/templates/partials/building/building-related-location-buildings.php
@@ -19,49 +19,55 @@
     // 'location_id' => $connected_location_id,
 // )); ?>
 
+<?php
+$query = $location_conncected_buildings;
+if ( $query->have_posts() ) : ?>
+    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+        <?php
+            $id = get_the_id();
 
-<?php foreach ($location_conncected_buildings->posts as $building) {
-    $id = $building->ID;
+            $main_content = Utils\ob_load_template_part('templates/snippets/' . $main_snippet, array(
+                'location_id' => $connected_location_id,
+                'building_id' => $id
+            ));
 
-    $main_content = Utils\ob_load_template_part('templates/snippets/' . $main_snippet, array(
-        'location_id' => $connected_location_id,
-        'building_id' => $id
-    ));
+            // image
+            //$sStyle = '';
+            if ( has_post_thumbnail($id) ) {
+                $image   = wp_get_attachment_image_url(get_post_thumbnail_id($id), 'width=500');
+                //$sStyle = sprintf(' style="background-image: url(%s)"', $sUrl);
+            } else {
+                $location_gallery       = get_field('carousel_images', $connected_location_id);
+                $image = $location_gallery[0]['url'];
+            }
 
-    // image
-    //$sStyle = '';
-    if ( has_post_thumbnail($id) ) {
-        $image   = wp_get_attachment_image_url(get_post_thumbnail_id($id), 'width=500');
-        //$sStyle = sprintf(' style="background-image: url(%s)"', $sUrl);
-    } else {
-        $location_gallery       = get_field('carousel_images', $connected_location_id);
-        $image = $location_gallery[0]['url'];
-    }
+            $title = get_the_title();
+            $url   = get_the_permalink();
 
-    $title = get_the_title($id);
-    $url   = get_the_permalink($id);
+            // build addresses
+            $address = join("\n", [
+                get_field('building_address_1'),
+                get_field('building_address_2'),
+                get_field('building_address_town_city'),
+                get_field('building_address_post_code')
+            ]);
 
-    // build addresses
-    $address = join("\n", [
-        get_field('building_address_1', $id),
-        get_field('building_address_2', $id),
-        get_field('building_address_town_city', $id),
-        get_field('building_address_post_code', $id)
-    ]);
+            // strip unneeded newlines
+            $address = trim(preg_replace("/\n\n+/", "\n", $address));
 
-    // strip unneeded newlines
-    $address = trim(preg_replace("/\n\n+/", "\n", $address));
+            $secondary_content = Utils\ob_load_template_part('templates/snippets/' . $secondary_snippet, array(
+                'title'   => $title,
+                'image'   => $image,
+                'url'     => $url,
+                'address' => $address
+            ));
 
-    $secondary_content = Utils\ob_load_template_part('templates/snippets/' . $secondary_snippet, array(
-        'title'   => $title,
-        'image'   => $image,
-        'url'     => $url,
-        'address' => $address
-    ));
-
-    echo Utils\ob_load_template_part('templates/components/split-feature', array(
-        'color'   => "sky",
-        'content' => $main_content,
-        'second'  => $secondary_content
-    ));
-} ?>
+            echo Utils\ob_load_template_part('templates/components/split-feature', array(
+                'color'   => "sky",
+                'content' => $main_content,
+                'second'  => $secondary_content
+            ));
+        ?>
+    <?php endwhile; ?>
+<?php endif; ?>
+<?php wp_reset_postdata(); ?>
