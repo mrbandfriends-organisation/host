fe957d26c7afb595214d669d6515b510d69d4e73
Rob Lowe <rob.lowe@mrbandfriends.co.uk>
2016-09-20 09:41:36 +0100

    COntact Page ACF Setup

--- site/web/app/themes/host/template-contact.php
+++ site/web/app/themes/host/template-contact.php
@@ -8,3 +8,5 @@
 <?php echo Utils\ob_load_template_part('templates/components/hero', array(
     'post_id'  => get_the_id()
 )); ?>
+
+<?php echo Utils\ob_load_template_part('templates/partials/contact/contact-intro'); ?>
---
+++ site/web/app/themes/host/templates/partials/contact/contact-intro.php
@@ -0,0 +1,31 @@
+<?php
+  /**
+  * CONTACT INTRO
+  **/
+    use Roots\Sage\Utils;
+?>
+
+
+<?php
+    $contact_title_1 = get_field('title_1');
+    $contact_title_2 = get_field('title_2');
+    $contact_description = get_field('description');
+?>
+
+<?php
+
+$main_content = Utils\ob_load_template_part('templates/snippets/building/introduction-text', array(
+    'title_1'           => $contact_title_1,
+    'title_2'           => $contact_title_2,
+    'description'       => $contact_description
+));
+
+$aside_content = Utils\ob_load_template_part('templates/snippets/building/introduction-aside');
+
+ ?>
+
+<?php echo Utils\ob_load_template_part('templates/components/billboard', array(
+    'color'         => 'sky',
+    'content'       => $main_content,
+    'second'        => $aside_content
+)); ?>
