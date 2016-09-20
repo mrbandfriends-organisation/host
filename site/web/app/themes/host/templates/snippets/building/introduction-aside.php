7fcf2fd186a5ef3266ab4b2d1ffa721244ce5d9e
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-20 16:17:59 +0100

    Working on adding svg icons to buttons

--- site/web/app/themes/host/templates/snippets/building/introduction-aside.php
+++ site/web/app/themes/host/templates/snippets/building/introduction-aside.php
@@ -2,27 +2,29 @@
     use Roots\Sage\Utils;
 ?>
 <div data-favouritable="<?=get_the_id(); ?>">
-    <?php
-      // check if the repeater field has rows of data
-      if( have_rows('links_downloads') ):?>
-
+    <?php // check if the repeater field has rows of data ?>
+    <?php if( have_rows('links_downloads') ):?>
         <h3 class="plain inherit-fg">Cant wait to get settled in? You’ll need these downloads</h3>
 
-          <?php while ( have_rows('links_downloads') ) : the_row();
-              // display a sub field value
-              $button_text = get_sub_field('button_text');
-              $link = get_sub_field('link');
-              $button_type = get_sub_field('button_type');
+        <?php while ( have_rows('links_downloads') ) : the_row();
+            $button_text     = get_sub_field('button_text');
+            $link            = get_sub_field('link');
+            //$button_type = ( !empty(get_sub_field('button_type')) && get_sub_field('button_type') === 'outline' ? 'btn--outline-only' : null );
+            $button_icon     = get_sub_field('button_icon');
+            $button_modifier = ( !empty($button_icon) ? 'btn--icon' : null );
         ?>
+            <?php if ( !empty($button_text) && !empty($link) ): ?>
+                <a href="<?php echo esc_html($link); ?>" class="btn btn--outline<?//= esc_attr($button_type);?> <?= esc_attr($button_modifier); ?>">
+                    <?php echo esc_html($button_text); ?>
 
-        <a href="<?php echo esc_html($link); ?>" class="btn <?php if ($button_type === 'outline'): echo esc_html('btn--outline-only'); endif; ?>">
-            <?php echo esc_html($button_text); ?>
-        </a>
+                    <?php echo Utils\ob_load_template_part('templates/partials/shared/icon', array(
+                        'icon'       => Utils\strip_file_format($button_icon),
+                        //"classnames" => "svg-icon--sky"
+                    )); ?>
+                </a>
+            <?php endif; ?>
 
-    <?php
-          endwhile;
-      else :
-          // no rows found
-      endif;
-    ?>
+        <?php endwhile; ?>
+
+    <?php endif; ?>
 </div>
