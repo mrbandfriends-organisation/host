c165f9d2995f4b989569d44ec1dfe555e06da578
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-20 12:52:33 +0100

    Updatewd get directions link

--- site/web/app/themes/host/templates/snippets/building/building-carousel-infobox.php
+++ site/web/app/themes/host/templates/snippets/building/building-carousel-infobox.php
@@ -1,4 +1,6 @@
 <?php
+    Use Roots\Sage\Extras;
+
     $building_name  = ( get_the_title() );
     $address_1      = ( !empty( get_field('building_address_1') ) ? get_field('building_address_1') : null );
     $town           = ( !empty( get_field('building_address_town_city') ) ? get_field('building_address_town_city') . ', ' : null );
@@ -11,6 +13,14 @@
     ]);
     // strip unneeded newlines
     $address = trim(preg_replace("/\n\n+/", "\n", $address));
+
+    // Address for the link to Google Maps
+    $google_maps_address = join( [
+        $address_1,
+        $town,
+        $post_code
+    ]);
+    $google_maps_address = str_replace(" ", '+', esc_html($address));
 ?>
 
 <?php if ( !empty($building_name) ): ?>
@@ -26,6 +36,13 @@
     <p>
         <?=str_replace("\n", '<br>', esc_html($address)); ?>
     </p>
+
+    <?php // Taking off to google maps with address as destination ?>
+    <strong class="carousel-infobox__subheading">
+        <a class="carousel-infobox__underlink-link" href="https://www.google.com/maps?daddr=<?= $google_maps_address ?>); ?>" <?php Extras\link_open_new_tab_attrs(); ?>>
+            Get directions
+        </a>
+    </strong>
 <?php endif; ?>
 
 <?php if ( !empty($phone) ): ?>
