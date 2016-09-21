6f62c95138922b21a3b01597082cb8a823becdd6
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-09-21 10:27:04 +0100

    Location what’s it like done on room

--- site/web/app/themes/host/templates/content-single-rooms.php
+++ site/web/app/themes/host/templates/content-single-rooms.php
@@ -27,7 +27,7 @@
 
   <?php echo Utils\ob_load_template_part('templates/partials/room/room-price-settings.php'); ?>
 
-  <?php echo Utils\ob_load_template_part('templates/partials/room/room-location.php'); ?>
+  <?php echo Utils\ob_load_template_part('templates/partials/room/room-location.php', compact('parent_building_id')); ?>
 
   <?php echo Utils\ob_load_template_part('templates/partials/shared/map', array(
       'iZoom' => get_field('map_zoom', $parent_building_id),
--- site/web/app/themes/host/templates/snippets/room/introduction-text.php
+++ site/web/app/themes/host/templates/snippets/room/introduction-text.php
@@ -3,6 +3,7 @@
 ?>
 
 <h2 class="h4">Available. 2 rooms remaining</h2>
+
 <h2>
   <?= esc_html($title_1); ?>.<br />
   From <?= esc_html($title_2); ?>
