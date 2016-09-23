aaa32b2dbfcd94b3b825484cf53fd21fa4986209
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-23 09:17:55 +0100

    Refactored CF for room and guilding ppints of interest

--- site/web/app/themes/host/templates/partials/room/room-location.php
+++ site/web/app/themes/host/templates/partials/room/room-location.php
@@ -6,17 +6,18 @@
 ?>
 
 <?php
-    $location_title = get_field('location_title', $parent_building_id);
-    $location_overview = get_field('location_description', $parent_building_id);
-    $location_city = 'London';
+    $location_title_1   = get_field('location_title_1', $parent_building_id);
+    $location_title_2   = get_field('location_title_2', $parent_building_id);
+    $location_overview  = get_field('location_description', $parent_building_id);
+    $location_city      = 'London';
+    $location_image     = get_field('location_image', $parent_building_id);
+    $locations          = get_field('points_of_interest', $parent_building_id);
 ?>
 
 <?php
-$main_content = Utils\ob_load_template_part('templates/snippets/building/things-to-do-introduction', compact('location_title', 'location_overview', 'location_city'));
-
-$aside_content = Utils\ob_load_template_part('templates/snippets/building/things-to-do-listing', compact('location_city'));
-
- ?>
+    $main_content = Utils\ob_load_template_part('templates/snippets/building/things-to-do-introduction', compact('location_title_1', 'location_title_2', 'location_overview', 'location_city'));
+    $aside_content = Utils\ob_load_template_part('templates/snippets/building/things-to-do-listing', compact('locations', 'location_city', 'location_image'));
+?>
 
 
 <?php
