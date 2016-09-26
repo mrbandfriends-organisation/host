99b6dce4ee5154f8c6662db78e173ccc78b828f8
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-09-26 14:47:46 +0100

    Added correct intro to Contact

--- site/web/app/themes/host/templates/partials/contact/contact-intro.php
+++ site/web/app/themes/host/templates/partials/contact/contact-intro.php
@@ -14,18 +14,18 @@
 
 <?php
 
-$main_content = Utils\ob_load_template_part('templates/snippets/building/introduction-text', array(
+$main_content = Utils\ob_load_template_part('templates/snippets/contact/introduction-text', array(
     'title_1'           => $contact_title_1,
     'title_2'           => $contact_title_2,
     'description'       => $contact_description
 ));
 
-$aside_content = Utils\ob_load_template_part('templates/snippets/building/introduction-aside');
+$aside_content = Utils\ob_load_template_part('templates/snippets/contact/introduction-aside');
 
  ?>
 
 <?php echo Utils\ob_load_template_part('templates/components/billboard', array(
-    'color'         => 'sky',
+    'color'         => 'orange',
     'content'       => $main_content,
     'second'        => $aside_content
 )); ?>
