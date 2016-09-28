acdaa09dbcde21a3a76e6b0b22b992b31792a848
David Smith <david.smith@mrbandfriends.co.uk>
2016-09-28 15:18:24 +0100

    Fix dependency on array.includes as not compatible with IE11

--- site/web/app/themes/host/assets/js/src/lib/FavouriteManager.js
+++ site/web/app/themes/host/assets/js/src/lib/FavouriteManager.js
@@ -1,6 +1,7 @@
 // jshint latedef:nofunc
-var cookies = require('cookies');
-var icon    = require('svg-icons');
+var cookies         = require('cookies');
+var icon            = require('svg-icons');
+var _includes       = require('lodash.includes');
 
 var sFavouriteTemplate =
 '<article class="box box--fg-{{availability.foreground}} favourites__favourite" data-id="{{id}}">'+
@@ -389,7 +390,7 @@ function FavouriteManager()
         elButton.addEventListener('click', function()
         {
             // a. if it contains it, remove
-            if (aiFavourites.includes(iId))
+            if ( _includes(aiFavourites, iId) )
             {
                 aiFavourites = aiFavourites.filter(function(iCurr)
                 {
