ea759a06323cf6fedd40eb5bf7166f1ebeb05513
David Smith <david.smith@mrbandfriends.co.uk>
2016-09-28 16:30:02 +0100

    Another logo test for IE11

--- site/web/app/themes/host/templates/partials/site-logo.php
+++ site/web/app/themes/host/templates/partials/site-logo.php
@@ -8,10 +8,5 @@
 ?>
 
 <a class="site-logo banner__home-link" href="<?php echo esc_url(home_url('/')); ?>" style="display: block; width: 200px;">
-	<picture>
-		<!--[if IE 9]><video style="display: none;"><![endif]-->
-		<source type="image/svg+xml" srcset="<?php echo esc_attr(get_stylesheet_directory_uri().'/assets/svg/unoptimised/site-logo.svg'); ?>">
-		<!--[if IE 9]></video><![endif]-->
-		<img src="<?php echo esc_attr(get_stylesheet_directory_uri().'/assets/svg/unoptimised/site-logo.png'); ?>" alt="<?php echo esc_attr(bloginfo('name'));?>">
-	</picture>
+	<img src="<?php echo esc_attr(get_stylesheet_directory_uri().'/assets/svg/unoptimised/site-logo.svg'); ?>">
 </a>
