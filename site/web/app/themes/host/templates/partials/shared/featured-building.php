a060d21490b492030b8aa99d875790ddbf3b51b6
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-28 10:48:31 +0100

    Correclty added conditional lofci to shared featrued buldings

--- site/web/app/themes/host/templates/partials/shared/featured-building.php
+++ site/web/app/themes/host/templates/partials/shared/featured-building.php
@@ -7,34 +7,41 @@
 
 <?php
     $featured_building                 = get_field('featured_building');
-    $featured_building_id              = $featured_building->ID;
-    $featured_building_title           = ( !empty($featured_building_title) ? $featured_building_title : get_field('featured_building_title') );
-    $featured_building_name            = $featured_building->post_title;
-    $featured_building_url             = $featured_building->guid;
-    $featured_building_description     = get_field('description', $featured_building_id);
-    $featured_building_carousel_images = get_field('carousel_images', $featured_building_id);
 
+    if ( !empty($featured_building) ) {
 
-    $main_content = Utils\ob_load_template_part('templates/snippets/shared/standard-content', array(
-        'title_1'     => $featured_building_title,
-        'title_2'     => $featured_building_name,
-        'content'     => $featured_building_description,
-        'button_url'  => $featured_building_url,
-        'button_text' => 'Show me this property'
-    ));
+        // Vars
+        $featured_building_id              = ( !empty($featured_building) ? $featured_building->ID : null );
+        $featured_building_title           = ( !empty($featured_building_title) ? $featured_building_title : get_field('featured_building_title') );
+        $featured_building_name            = $featured_building->post_title;
+        $featured_building_url             = $featured_building->guid;
+        $featured_building_description     = get_field('description', $featured_building_id);
+        $featured_building_carousel_images = get_field('carousel_images', $featured_building_id);
 
-
-    if ( is_front_page() ) {
-        $second_content = Utils\ob_load_template_part('templates/partials/shared/stacked-gallery', array(
-            'images'        => $featured_building_carousel_images,
-            'grid_modifier' => 'grid--vertical-l'
+        
+        $main_content = Utils\ob_load_template_part('templates/snippets/shared/standard-content', array(
+            'title_1'     => $featured_building_title,
+            'title_2'     => $featured_building_name,
+            'content'     => $featured_building_description,
+            'button_url'  => $featured_building_url,
+            'button_text' => 'Show me this property'
         ));
-    } else {
-        $second_content = Utils\ob_load_template_part('templates/partials/uni/uni-featured-building-second', array(
-            'connected_location'   => host_universities_find_connected_location( get_the_id() ),
-            'featuerd_building_id' => $featured_building_id
-        ));
+
+
+        if ( is_front_page() ) {
+            $second_content = Utils\ob_load_template_part('templates/partials/shared/stacked-gallery', array(
+                'images'        => $featured_building_carousel_images,
+                'grid_modifier' => 'grid--vertical-l'
+            ));
+        } else {
+            $second_content = Utils\ob_load_template_part('templates/partials/uni/uni-featured-building-second', array(
+                'connected_location'   => host_universities_find_connected_location( get_the_id() ),
+                'featuerd_building_id' => $featured_building_id
+            ));
+        }
     }
+
+
 ?>
 
 <?php if( $featured_building ): ?>
