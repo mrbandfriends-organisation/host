3ae09376fe1dda72d882c6b5c8708657840f6bc9
David Smith <david.smith@mrbandfriends.co.uk>
2016-09-28 16:26:29 +0100

    Temporary logo test for IE11

--- site/web/app/themes/host/templates/partials/site-logo.php
+++ site/web/app/themes/host/templates/partials/site-logo.php
@@ -1,18 +1,17 @@
 <?php
 /**
-* SITE LOGO
+* SITE LOGO.
 *
 * uses RIMG (via Picturefill or Native) to load either SVG or PNG fallback
 * of the logo...
 */
-
 ?>
 
-<a class="site-logo banner__home-link" href="<?php echo esc_url( home_url('/') ); ?>">
+<a class="site-logo banner__home-link" href="<?php echo esc_url(home_url('/')); ?>" style="display: block; width: 200px;">
 	<picture>
 		<!--[if IE 9]><video style="display: none;"><![endif]-->
-		<source type="image/svg+xml" srcset="<?php echo esc_attr( get_stylesheet_directory_uri() . '/assets/svg/standalone/output/site-logo.svg'); ?>">
+		<source type="image/svg+xml" srcset="<?php echo esc_attr(get_stylesheet_directory_uri().'/assets/svg/unoptimised/site-logo.svg'); ?>">
 		<!--[if IE 9]></video><![endif]-->
-		<img src="<?php echo esc_attr(get_stylesheet_directory_uri() .'/assets/svg/standalone/output/png/site-logo.png'); ?>" alt="<?php echo esc_attr(bloginfo('name'));?>">
+		<img src="<?php echo esc_attr(get_stylesheet_directory_uri().'/assets/svg/unoptimised/site-logo.png'); ?>" alt="<?php echo esc_attr(bloginfo('name'));?>">
 	</picture>
 </a>
