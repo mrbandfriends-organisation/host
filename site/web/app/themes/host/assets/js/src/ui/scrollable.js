e81801ce1c94c4b33f5fc44a554035fdd310d7e9
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-29 12:37:44 +0100

    Added fix to “hide” scrollable scriton on building page on smaller viewports

--- site/web/app/themes/host/assets/js/src/ui/scrollable.js
+++ site/web/app/themes/host/assets/js/src/ui/scrollable.js
@@ -4,5 +4,25 @@ module.exports = function()
 {
     "use strict";
 
-    $('.js-scrollable').perfectScrollbar();
+    // If is large viewport or up use perfectScrollbar
+    if ( window.innerWidth > 990 ) {
+        $('.js-scrollable').perfectScrollbar();
+    }
+
+    // If its a smaller viewport
+    else {
+
+        // 1. Add class to allow for unlimited max height
+        var scrollable = document.querySelector('.js-scrollable');
+        scrollable.classList.add("scrollable--mobile");
+
+        // 2. If there are more than 3 locations then hide
+        //    all locations above 3
+        if (scrollable.children.length > 3) {
+            for (var i = 3; i < scrollable.children.length; i++) {
+                scrollable.children[i].classList.add("vh");
+            }
+        }
+    }
+
 };
