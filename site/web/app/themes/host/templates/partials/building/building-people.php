60a138f34f55ee5e86efd707873e16049fac9feb
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-30 11:44:44 +0100

    Correctly conditionalised building person partial

--- site/web/app/themes/host/templates/partials/building/building-people.php
+++ site/web/app/themes/host/templates/partials/building/building-people.php
@@ -12,30 +12,28 @@
 ?>
 
 <?php
-  // People Gallery
-  if( have_rows('people_photos') ):
+    // People Gallery
+    if( have_rows('people_photos') ):
 
-      while ( have_rows('people_photos') ) : the_row();
-
-          $photo = get_sub_field('photo');
-          $photo_title = $photo['title'];
-          $photo_url = $photo['url'];
-?>
-
-<?php
-      endwhile;
-  else :
-      // no rows found
-  endif;
+        while ( have_rows('people_photos') ) : the_row();
+            $photo       = ( !empty(get_sub_field('photo')) ? get_sub_field('photo') : null );
+            $photo_title = ( !empty($photo) ? $photo['title'] : null );
+            $photo_url   = ( !empty($photo) ? $photo['url'] : null );
+        endwhile;
+    endif;
 ?>
 
 <?php
 $main_content = Utils\ob_load_template_part('templates/snippets/building/our-people-introduction', compact('people_title', 'people_description'));
 
-$aside_content = Utils\ob_load_template_part('templates/components/bleed-image', array(
-    'image'  => $photo_url
-));
- ?>
+if ( !empty($photo_url) ) {
+    $aside_content = Utils\ob_load_template_part('templates/components/bleed-image', array(
+        'image'  => $photo_url
+    ));
+} else {
+    $aside_content = null;
+}
+?>
 
 <?php
 echo Utils\ob_load_template_part('templates/components/split-feature', [
