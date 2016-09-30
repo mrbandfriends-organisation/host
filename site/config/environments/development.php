4fdc59f532ae4afaf8d855267767e0b3e5a1031f
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-09-30 12:38:12 +0100

    Allowed for x-domain tracking

--- site/config/environments/development.php
+++ site/config/environments/development.php
@@ -4,3 +4,4 @@ define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
 define('GMAPS_API_KEY', 'AIzaSyDOkwRPoKPiEjQ9KwKz0i9WA7JuMpcltQM');
+define('GOOGLE_ANALYTICS_CODE', 'UA-84824698-1');
