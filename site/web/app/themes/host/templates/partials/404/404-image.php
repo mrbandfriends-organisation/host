5d40c69657a1bb3dc3ea4d853ee22905ac870503
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-09-30 11:28:33 +0100

    Created 404 page

---
+++ site/web/app/themes/host/templates/partials/404/404-image.php
@@ -0,0 +1,40 @@
+<?php
+    $class = ( !empty($class) ? $class : null );
+?>
+
+<svg class="<?php echo esc_attr($class); ?>" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="375" height="443" viewBox="0 0 375 443">
+  <defs>
+    <linearGradient id="d6b31fe8-d261-42bb-9e25-e21a3d80c58f" data-name="New Gradient Swatch 5" x1="187.5" y1="91.38" x2="187.5" y2="362.62" gradientTransform="translate(409.01 34.01) rotate(90)" gradientUnits="userSpaceOnUse">
+      <stop offset="0" stop-color="#fcf479"/>
+      <stop offset="0.16" stop-color="#fcf274"/>
+      <stop offset="0.35" stop-color="#fcea66"/>
+      <stop offset="0.56" stop-color="#fcdf4e"/>
+      <stop offset="0.78" stop-color="#fbce2d"/>
+      <stop offset="1" stop-color="#fbb903"/>
+    </linearGradient>
+    <linearGradient id="c9b6bb43-1eed-4770-82b3-d318ff71d4b6" data-name="New Gradient Swatch 4" x1="136.41" y1="219.21" x2="136.41" y2="326.99" gradientTransform="translate(8.02 -6.55) rotate(2.04)" gradientUnits="userSpaceOnUse">
+      <stop offset="0" stop-color="#7ac3e2"/>
+      <stop offset="1" stop-color="#4bb5e2"/>
+    </linearGradient>
+    <linearGradient id="9bf806d5-dbb4-4bb8-a1d8-1a64ed8ccfd6" x1="238.59" y1="219.21" x2="238.59" y2="326.99" xlink:href="#c9b6bb43-1eed-4770-82b3-d318ff71d4b6"/>
+  </defs>
+  <title>404_pause</title>
+  <g style="isolation: isolate">
+    <g id="5a43eb9e-44e4-4ab9-896e-c162467e6322" data-name="Layer 1">
+      <rect width="99" height="443" style="fill: #55beee"/>
+      <rect x="276" width="99" height="443" style="fill: #55beee"/>
+      <g id="f91c88e9-b0da-4af7-8898-f97d681b7c2f" data-name="&lt;Group&gt;">
+        <image width="327" height="327" transform="translate(23.86 78)" xlink:href="404_pause1.png" style="opacity: 0.35000000000000003;mix-blend-mode: multiply"/>
+        <g>
+          <circle cx="187.5" cy="221.51" r="116.74" transform="translate(-40.56 400.99) rotate(-87.96)" style="fill: url(#d6b31fe8-d261-42bb-9e25-e21a3d80c58f)"/>
+          <path d="M192.24,88.79C119,86.17,57.39,143.61,54.78,216.77s54.79,134.82,128,137.44,134.85-54.77,137.46-128S265.41,91.4,192.24,88.79ZM70.84,217.35a116.74,116.74,0,1,1,112.5,120.83A116.73,116.73,0,0,1,70.84,217.35Z" style="fill: #fff"/>
+          <path d="M118.14,213.87l-3.51,98.22a116.36,116.36,0,0,0,36.3,20.42l4.19-117.32Z" style="fill: url(#c9b6bb43-1eed-4770-82b3-d318ff71d4b6)"/>
+          <path d="M220.25,217.51,216,335.67a116.57,116.57,0,0,0,37.61-16.4l3.59-100.44Z" style="fill: url(#9bf806d5-dbb4-4bb8-a1d8-1a64ed8ccfd6)"/>
+          <path d="M184.22,311.3h0a24,24,0,0,1-23.16-24.86l.39-10.93a24,24,0,0,1,24.88-23.17h0a24,24,0,0,1,23.17,24.88l-.39,10.93A24,24,0,0,1,184.22,311.3Z" style="fill: #42210b"/>
+          <path d="M163.42,220.3l-56.27-2a4.81,4.81,0,0,1,.34-9.62l56.27,2a4.81,4.81,0,0,1-.34,9.62Z" style="fill: #42210b"/>
+          <path d="M266.7,224l-56.27-2a4.81,4.81,0,0,1,.34-9.62l56.27,2a4.81,4.81,0,0,1-.34,9.62Z" style="fill: #42210b"/>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
