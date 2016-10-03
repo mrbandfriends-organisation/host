d3304d0c24a7e4dab242721e06bad5fae310c624
Stuart Nelson <stuart.nelson@mrbandfriends.co.uk>
2016-10-03 15:45:41 +0100

    Revert "Merge branch 'release/1.4.0'"
    This reverts commit be0cfccbcbcada8a92da8257955640e751d7c388, reversing
    changes made to 510f2ce7dd6220794fd6aa1d60b0046b4d7b1161.
    
    # Conflicts:
    #	site/web/app/themes/host/assets/css/main.min.css

--- site/web/app/themes/host/assets/js/dist/app.js
+++ site/web/app/themes/host/assets/js/dist/app.js
@@ -13355,37 +13355,35 @@
 	 * by default the window won't scroll down to show any
 	 * error messages display
 	 */
-	module.exports = function() {
-	    (function() {
-	        'use strict';
+	(function() {
+	    'use strict';
 
-	        // Check for a validation notice (success / fail)
-	        // .add() adds an additional selector to the result set thereby
-	        // allowing us to test for x2 selectors due to GForm variations
-	        var validationNotice = $('.gform_confirmation_message').add('form .validation_error');
+	    // Check for a validation notice (success / fail)
+	    // .add() adds an additional selector to the result set thereby
+	    // allowing us to test for x2 selectors due to GForm variations
+	    var validationNotice = $('.gform_confirmation_message').add('form .validation_error');
 
-	        if ( validationNotice.length ) {
+	    if ( validationNotice.length ) {
 
-	            // Get the closest parent "div" - only way to be consistent with GForms
-	            var formWrapper = validationNotice.parent().closest('div');
+	        // Get the closest parent "div" - only way to be consistent with GForms
+	        var formWrapper = validationNotice.parent().closest('div');
 
-	            var id  = formWrapper.attr('id');
+	        var id  = formWrapper.attr('id');
 
-	            // If the form is within a toogled element then show that element
-	            var toggleWrapper = validationNotice.parents('.js-toggle-form');
+	        // If the form is within a toogled element then show that element
+	        var toggleWrapper = validationNotice.parents('.js-toggle-form');
 
-	            if (toggleWrapper.length) {
-	                toggleWrapper.show();
-	            }
-
-	            // Skip to the document point
-	            if (id !== undefined) {
-	                window.location.hash = id;
-	            }
+	        if (toggleWrapper.length) {
+	            toggleWrapper.show();
 	        }
 
-	    });
-	};
+	        // Skip to the document point
+	        if (id !== undefined) {
+	            window.location.hash = id;
+	        }
+	    }
+
+	}());
 
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
 
