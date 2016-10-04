8e88dc07903d01e8809ea9603ef23ae21fc8e72a
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-10-04 10:59:17 +0100

    Fixed issues with tag manager script include

--- site/web/app/themes/host/lib/tracking.php
+++ site/web/app/themes/host/lib/tracking.php
@@ -10,20 +10,12 @@ use Roots\Sage\Utils;
  */
 function google_tag_manager_code() {?>
   <?php if ( defined( 'GOOGLE_TAG_MANAGER_CODE' ) && !empty( GOOGLE_TAG_MANAGER_CODE )) { ?>
-    <script type="text/javascript">
-        window.dataLayer = window.dataLayer || [];
-        dataLayer.push({
-    	     'gaProfileId': '<?php echo GOOGLE_TAG_MANAGER_CODE ?>'
-        });
-    </script>
     <!-- Google Tag Manager -->
-    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KJP9LC"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KJP9LC');</script>
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','<?php echo GOOGLE_TAG_MANAGER_CODE ?>');</script>
     <!-- End Google Tag Manager -->
   <?php } ?>
 <?php
