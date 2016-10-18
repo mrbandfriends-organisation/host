81c7968b9bce2ed689f92f7ce59e3f4e74dd49fa
Agnieszka <agnieszka.matuszak@mrbandfriends.co.uk>
2016-10-18 16:56:04 +0100

    Adding alt tags for logo, map, room (in the location), and footer sections

--- site/web/app/themes/host/templates/partials/site-logo.php
+++ site/web/app/themes/host/templates/partials/site-logo.php
@@ -9,5 +9,5 @@
 
 <a class="site-logo banner__home-link" href="<?php echo esc_url(home_url('/')); ?>">
 	<?php // note we're using an unoptimised SVG because the optimised version looked TERRIBLE in IE11 which isn't so good at rendering ?>
-	<img src="<?php echo esc_attr(get_stylesheet_directory_uri().'/assets/svg/unoptimised/site-logo.svg'); ?>">
+	<img src="<?php echo esc_attr(get_stylesheet_directory_uri().'/assets/svg/unoptimised/site-logo.svg'); ?>" alt="Host Students - Where students are at home">
 </a>
