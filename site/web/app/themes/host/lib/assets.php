c2b571a22cae936002ab1689c5eed0ac95ac3661
Cliff Williams <cliff@mrbandfriends.co.uk>
2016-10-21 16:15:55 +0100

    Updated get_responsive_image to include an alt tag

--- site/web/app/themes/host/lib/assets.php
+++ site/web/app/themes/host/lib/assets.php
@@ -156,82 +156,92 @@ add_filter('script_loader_src', __NAMESPACE__.'\\bust_caching');
  * @param   image_id    the ID of the attachment to generate markup for
  * @param   aConf       a hash of options
  */
-function get_responsive_image( $image_ref, $aConf = [] )
-{
-    // 0. sanity check our input to make sure we’re not being passed null/a non-attachment
-    if ( ($image_ref === null) )
-    {
-        return '';
-    }
+ function get_responsive_image( $image_ref, $aConf = [] )
+ {
+     // 0. sanity check our input to make sure we’re not being passed null/a non-attachment
+     if ( ($image_ref === null) )
+     {
+         return '';
+     }
 
-    // 1. get some defaults
-    $aConf = array_merge([
-        'dimensions'   => [ 1200, 768, 600, 320 ],
-        'default_dim'  => 600,
-        'aspect_ratio' => null,
-        'class'        => 'attachment-thumbnail',
-        'wpthumb'      => '&crop=true'
-    ], $aConf);
+     // 1. get some defaults
+     $aConf = array_merge([
+         'dimensions'   => [ 1200, 768, 600, 320 ],
+         'default_dim'  => 600,
+         'aspect_ratio' => null,
+         'class'        => 'attachment-thumbnail',
+         'wpthumb'      => '&crop=true',
+         'alt'          => '' // empty string is the minimumal requirement for alt
+     ], $aConf);
 
-    // 2. basic HTML attributes
-    $aAttr = $aConf;
-    unset($aAttr['dimensions'], $aAttr['default_dim'], $aAttr['wpthumb'], $aAttr['aspect_ratio']);  // assume everything else goes to HTML
+     // 2. basic HTML attributes
+     $aAttr = $aConf;
+     unset($aAttr['dimensions'], $aAttr['default_dim'], $aAttr['wpthumb'], $aAttr['aspect_ratio']);  // assume everything else goes to HTML
 
-    // 3. set appropriate aspect ratio
-    $sDim = "width={$aConf['default_dim']}{$aConf['wpthumb']}";
-    if ($aConf['aspect_ratio'] !== null)
-    {
-        $sDim .= '&height='.round($aConf['default_dim'] / $aConf['aspect_ratio']);
-    }
+     // 3. set appropriate aspect ratio
+     $sDim = "width={$aConf['default_dim']}{$aConf['wpthumb']}";
+     if ($aConf['aspect_ratio'] !== null)
+     {
+         $sDim .= '&height='.round($aConf['default_dim'] / $aConf['aspect_ratio']);
+     }
 
-    // 3.5 - get a valid src either from the attachment or using the src if this has been passed directly
-    if ( !absint( $image_ref ) || ( get_post_type( $image_ref ) !== 'attachment' ) ) {
-        $is_direct_src = true;
-        $aAttr['src'] = $image_ref; // it's a direct image src already
-    } else {
-        $is_direct_src = false;
-        $aAttr['src'] = wp_get_attachment_image_url($image_ref, $sDim);
-    }
+     // 3.5 - get a valid src either from the attachment or using the src if this has been passed directly
+     if ( !absint( $image_ref ) || ( get_post_type( $image_ref ) !== 'attachment' ) ) {
+         $is_direct_src = true;
+         $aAttr['src'] = $image_ref; // it's a direct image src already
+         $aAttr['alt'] = $aConf['alt'];
+     } else {
+         $is_direct_src = false;
 
-    // 4. srcset
-    $aSrcset = [];
-    foreach ($aConf['dimensions'] AS $iSize)
-    {
-        // a. dimension string for WPThumb
-        $sDim = "width={$iSize}{$aConf['wpthumb']}";
+         if (empty( $aConf['alt'] ) ) {
+             $aAttr['alt'] = Utils\get_image_alt_by_id( $image_ref );
+         } else {
+             $aAttr['alt'] = $aConf['alt'];
+         }
 
-        // b. if there’s an aspect ratio…
-        if ($aConf['aspect_ratio'] !== null)
-        {
-            $sDim .= '&height='.round($iSize / $aConf['aspect_ratio']);
-        }
+         $aAttr['src'] = wp_get_attachment_image_url($image_ref, $sDim);
+     }
 
-        // c. call out
-        if ($is_direct_src) {
-            $aSrcset[] = wpthumb($image_ref, $sDim)." {$iSize}w";
-        } else {
-            $aSrcset[] = wp_get_attachment_image_url($image_ref, $sDim)." {$iSize}w";
-        }
-    }
-    if (count($aSrcset) > 0)
-    {
-        $aAttr['srcset'] = join(', ', $aSrcset);
-    }
-    unset($aSrcset);
+     // 4. srcset
+     $aSrcset = [];
+     foreach ($aConf['dimensions'] AS $iSize)
+     {
+         // a. dimension string for WPThumb
+         $sDim = "width={$iSize}{$aConf['wpthumb']}";
+
+         // b. if there’s an aspect ratio…
+         if ($aConf['aspect_ratio'] !== null)
+         {
+             $sDim .= '&height='.round($iSize / $aConf['aspect_ratio']);
+         }
+
+         // c. call out
+         if ($is_direct_src) {
+             $aSrcset[] = wpthumb($image_ref, $sDim)." {$iSize}w";
+         } else {
+             $aSrcset[] = wp_get_attachment_image_url($image_ref, $sDim)." {$iSize}w";
+         }
+     }
+     if (count($aSrcset) > 0)
+     {
+         $aAttr['srcset'] = join(', ', $aSrcset);
+     }
+     unset($aSrcset);
 
 
-    // 4.5 - ensure we have a sizes attribute if the user didn't provide
-    if ( empty( $aConf['sizes'] ) && !empty($aConf['dimensions']))
-    {
-        $largest_image = max( $aConf['dimensions'] );
-        $aAttr['sizes'] = "(max-width: {$largest_image}px) 100vw, {$largest_image}px";
-    }
+     // 4.5 - ensure we have a sizes attribute if the user didn't provide
+     if ( empty( $aConf['sizes'] ) && !empty($aConf['dimensions']))
+     {
+         $largest_image = max( $aConf['dimensions'] );
+         $aAttr['sizes'] = "(max-width: {$largest_image}px) 100vw, {$largest_image}px";
+     }
 
 
-    // 5. output
-    return  Utils\tag('img', $aAttr);
+     // 5. output
+     return  Utils\tag('img', $aAttr);
 
-}
+ }
+
 
 /**
  * Resonsive alternative to the_post_thumbnail.
