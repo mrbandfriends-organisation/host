e3695486a43fbc160c70d317e8b19e2b882df34f
Stuart Nelson <stuart@northernbadger.co.uk>
2016-11-15 14:42:35 +0000

    Refactored header carosuel with slick

--- site/web/app/themes/host/templates/partials/shared/header-carousel.php
+++ site/web/app/themes/host/templates/partials/shared/header-carousel.php
@@ -3,6 +3,7 @@
     * GLOABL - HEADER CAROUSEL PARTIAL
     **/
     use Roots\Sage\Utils;
+    use Roots\Sage\Assets;
 
     $carousel_images = get_field('carousel_images');
     $info_box = ( !empty($info_box) ? $info_box : null );
@@ -14,16 +15,20 @@
     }
 ?>
 
-<div class="band slideshow js-slideshow slideshow--large-pagination slideshow--light-pagination slideshow--animate-in" data-pagination="pn dots">
-    <ul class="slideshow__list js-slideshow__list">
-        
+<div class="band">
+    <ul class="slick-header-carousel js-slick-header-carousel">
+
         <?php echo $info_box; ?>
 
         <?php foreach ( $carousel_images AS $image): ?>
-            <li class="slideshow__item js-slideshow__item" style="background-image:url(<?=$image['sizes']['large'];?>);">
-                <img src="<?php echo $image['sizes']['large']; ?>" class="slideshow__image js-slideshow__image" alt="<?=$image['alt']; ?>">
-
+            <li class="slick-header-carousel__slide js-rimgbg">
+                <?php echo Assets\get_responsive_image($image['id'], array()); ?>
             </li>
         <?php endforeach; ?>
     </ul>
 </div>
+
+
+<!-- <li class="slideshow__item js-slideshow__item" style="background-image:url(<?//=$image['sizes']['large'];?>);">
+    <img src="<?php //echo $image['sizes']['large']; ?>" class="slideshow__image js-slideshow__image" alt="<?//=$image['alt']; ?>">
+</li> -->
