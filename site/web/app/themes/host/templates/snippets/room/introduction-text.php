f3f1dd54f11dfc083aee4ec899db9efff9c58f51
Stuart J Nelson <stuart@northernbadger.co.uk>
2016-11-25 15:29:59 +0000

    BH#256: Fixed way title is being generated for room to correct colours/layout

--- site/web/app/themes/host/templates/snippets/room/introduction-text.php
+++ site/web/app/themes/host/templates/snippets/room/introduction-text.php
@@ -3,8 +3,9 @@
 ?>
 
 <h2>
-  <?= esc_html($title_1); ?>.<br />
-  From <?= esc_html($title_2); ?>
+  <?= esc_html($title_1); ?><br />
+  <?= esc_html($title_2); ?>.<br />
+  From <?= esc_html($title_3); ?>
 </h2>
 
 <?= Utils\esc_textarea__($description); ?>
