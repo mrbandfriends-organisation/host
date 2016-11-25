5261ebf223520846ab5316073328e043d102355f
Stuart J Nelson <stuart@northernbadger.co.uk>
2016-11-25 13:55:27 +0000

    Fixed if no location info is added then icon wont apear

--- site/web/app/themes/host/templates/snippets/building/things-to-do-listing.php
+++ site/web/app/themes/host/templates/snippets/building/things-to-do-listing.php
@@ -9,34 +9,40 @@
 
             <ul class="transport-list scrollable js-scrollable">
                 <?php foreach( $locations as $location ): ?>
-                    <li class="transport-list__item">
-                        <h3 class="transport-list__heading h3"><?= esc_html($location['title']); ?></h3>
-                        <ul class="transport-list__listing">
-                            <li class="transport-list__listing-item">
-                                <?php echo Utils\ob_load_template_part('templates/partials/shared/icon', array(
-                                    'icon'       => 'travel-public-bus',
-                                    "classnames" => "transport-list__icon svg-icon--sky"
-                                )); ?>
+                    <?php if ( !empty($location['title']) ): ?>
+                        <li class="transport-list__item">
+                            <h3 class="transport-list__heading h3"><?= esc_html($location['title']); ?></h3>
+                            <ul class="transport-list__listing">
+                                <?php if ( !empty($location['transport_time']) ): ?>
+                                    <li class="transport-list__listing-item">
+                                        <?php echo Utils\ob_load_template_part('templates/partials/shared/icon', array(
+                                            'icon'       => 'travel-public-bus',
+                                            "classnames" => "transport-list__icon svg-icon--sky"
+                                        )); ?>
 
-                                <div class="transport-list__content">
-                                    <?= esc_html($location['transport_time']); ?> <br>
-                                    Public transport
-                                </div>
+                                        <div class="transport-list__content">
+                                            <?= esc_html($location['transport_time']); ?> <br>
+                                            Public transport
+                                        </div>
+                                    </li>
+                                <?php endif; ?>
 
-                            </li>
-                            <li class="transport-list__listing-item">
-                                <?php echo Utils\ob_load_template_part('templates/partials/shared/icon', array(
-                                    'icon'       => 'travel-walking',
-                                    "classnames" => "transport-list__icon svg-icon--sky"
-                                )); ?>
+                                <?php if ( !empty($location['walking_time']) ): ?>
+                                    <li class="transport-list__listing-item">
+                                        <?php echo Utils\ob_load_template_part('templates/partials/shared/icon', array(
+                                            'icon'       => 'travel-walking',
+                                            "classnames" => "transport-list__icon svg-icon--sky"
+                                        )); ?>
 
-                                <div class="transport-list__content">
-                                    <?= esc_html($location['walking_time']); ?><br>
-                                    Walking time
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
+                                        <div class="transport-list__content">
+                                            <?= esc_html($location['walking_time']); ?><br>
+                                            Walking time
+                                        </div>
+                                    </li>
+                                <?php endif; ?>
+                            </ul>
+                        </li>
+                    <?php endif; ?>
                 <?php endforeach; ?>
             </ul>
         </div>
