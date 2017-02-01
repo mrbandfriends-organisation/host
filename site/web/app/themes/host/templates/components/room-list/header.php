ca6b62a707a89e2a9de804bc00b3b65ee9b280b1
Cliff Williams <cliff@mrbandfriends.co.uk>
2017-02-01 17:27:04 +0000

    Added anchors to room type listing

--- site/web/app/themes/host/templates/components/room-list/header.php
+++ site/web/app/themes/host/templates/components/room-list/header.php
@@ -8,13 +8,15 @@
             <h3>Take a look at<br>our room types</h3>
 
             <ul class="room-list-nav__list separated-list">
+            <?php $count = 1; ?>
             <?php while ( $rooms->have_posts() ) : $rooms->the_post(); ?>
                 <?php $id = get_the_id(); ?>
                 <?php $idx = substr(uniqid(), 0, 6); ?>
 
                 <li class="room-list-nav__item separated-list__item">
-                    <a href="#<?="r{$id}"; ?>" class="room-list-nav__link separated-list__link"><?=get_the_title(); ?></a>
+                    <a href="#room-<?= esc_attr($count); ?>" class="room-list-nav__link separated-list__link"><?=get_the_title(); ?></a>
                 </li>
+                <?php $count++; ?>
             <?php endwhile; ?>
             </ul>
         </nav>
--- site/web/app/themes/host/templates/components/room-list/index.php
+++ site/web/app/themes/host/templates/components/room-list/index.php
@@ -7,10 +7,12 @@
 
     <ul class="room-list__list">
 
+        <?php $count = 1; ?>
         <?php while ( $rooms->have_posts() ) : $rooms->the_post(); ?>
-            <li class="room-list__item">
+            <li id="room-<?= esc_attr($count); ?>" class="room-list__item">
                 <?php echo Utils\ob_load_template_part('templates/components/room-list/room'); ?>
             </li>
+            <?php $count++; ?>
         <?php endwhile; ?>
         <?php wp_reset_postdata(); ?>
 
