3a3c02cacac189e80864a75298a4408f5d6f9982
Cliff Williams <cliff@mrbandfriends.co.uk>
2017-02-02 16:06:56 +0000

    Checked for parent URL before showing breadcrumb

--- site/web/app/themes/host/templates/partials/room/room-breadcrumb.php
+++ site/web/app/themes/host/templates/partials/room/room-breadcrumb.php
@@ -1,9 +1,13 @@
-<nav class="breadcrumbs island">
-    <ul class="container grid">
-        <li class="breadcrumbs__item">
-            <a href="<?= esc_attr($parent_url); ?>" class="breadcrumbs__link">
-                < Back to building
-            </a>
-        </li>
-    </ul>
-</nav>
+<?php if ( !empty($parent_url) ): ?>
+
+    <nav class="breadcrumbs island">
+        <ul class="container grid">
+            <li class="breadcrumbs__item">
+                <a href="<?= esc_attr($parent_url); ?>" class="breadcrumbs__link">
+                    < Back to building
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+<?php endif; ?>
