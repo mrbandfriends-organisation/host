dccc08aa751fcc6e6268ed3e952ac807809cf80d
David Smith <david.smith@mrbandfriends.co.uk>
2017-02-03 16:28:58 +0000

    Add ability to randomise header carousel slides via CMS

--- site/web/app/themes/host/assets/js/src/ui/slick.js
+++ site/web/app/themes/host/assets/js/src/ui/slick.js
@@ -1,4 +1,34 @@
 require('slick-carousel');
+
+/**
+ * SHUFFLE
+ * shuffle some DOM elements
+ * https://css-tricks.com/snippets/jquery/shuffle-dom-elements/
+ */
+(function($){ 
+    $.fn.shuffle = function() {
+ 
+        var allElems = this.get(),
+            getRandom = function(max) {
+                return Math.floor(Math.random() * max);
+            },
+            shuffled = $.map(allElems, function(){
+                var random = getRandom(allElems.length),
+                    randEl = $(allElems[random]).clone(true)[0];
+                allElems.splice(random, 1);
+                return randEl;
+           });
+ 
+        this.each(function(i){
+            $(this).replaceWith($(shuffled[i]));
+        });
+ 
+        return $(shuffled);
+ 
+    }; 
+})(jQuery);
+
+
 module.exports = function()
 {
     $('.js-slick-fade').slick({
@@ -23,21 +53,32 @@ module.exports = function()
        ]
     });
 
-    /**
-     *  SLICK HEADER CAROSUEL
-     */
-    $('.js-slick-header-carousel').slick({
-        arrows: true,
-        dots: true,
-        infinite: true,
-        mobileFirst: true,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    draggable: false
+    (function() {
+        /**
+         *  SLICK HEADER CAROSUEL
+         */
+        
+        var carousel        = $('.js-slick-header-carousel');
+        var shouldShuffle   = carousel.data('shuffle');
+        var slides          = carousel.find('.js-slick-header-carousel-slide')
+
+        if (shouldShuffle) { // if marked as ranomise in CMS then shuffle the slides prior to init slick
+            slides.shuffle();
+        }
+
+        $('.js-slick-header-carousel').slick({
+            arrows: true,
+            dots: true,
+            infinite: true,
+            mobileFirst: true,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        draggable: false
+                    }
                 }
-            }
-        ]
-    });
+            ]
+        });
+    }());
 }
