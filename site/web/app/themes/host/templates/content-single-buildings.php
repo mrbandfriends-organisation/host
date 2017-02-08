96b8d6a80b8b676fa275ee39ee279d6e3701eb56
David Smith <david.smith@mrbandfriends.co.uk>
2017-02-08 16:49:58 +0000

    Add Building level social links to carousel infobox

--- site/web/app/themes/host/templates/content-single-buildings.php
+++ site/web/app/themes/host/templates/content-single-buildings.php
@@ -5,17 +5,30 @@
     <?php
         $connected_location = host_building_find_connected_location(get_the_id())->post;
         $connected_location_name = $connected_location->post_title;
+
+        $twitter_link       = get_field('building_twitter');
+        $facebook_link      = get_field('building_facebook');
+        $instagram_link     = get_field('building_instagram');
+
+
     ?>
 
     <?php echo Utils\ob_load_template_part('templates/partials/shared/header-carousel', array(
+            
             'info_box' => Utils\ob_load_template_part('templates/snippets/shared/carousel-infobox', array(
-            'building_name'     => get_the_title(),
-            'address_1'         => get_field('building_address_1'),
-            'town'              => get_field('building_address_town_city'),
-            'post_code'         => get_field('building_address_post_code'),
-            'phone'             => get_field('building_address_phone_no'),
-            'email'             => get_field('building_email_address'),
-        ))
+                'building_name'     => get_the_title(),
+                'address_1'         => get_field('building_address_1'),
+                'town'              => get_field('building_address_town_city'),
+                'post_code'         => get_field('building_address_post_code'),
+                'phone'             => get_field('building_address_phone_no'),
+                'email'             => get_field('building_email_address'),
+                'social_links'      => array(
+                    'twitter'   => $twitter_link,
+                    'facebook'  => $facebook_link,
+                    'instagram' => $instagram_link
+                )
+            ) // end of attrs passed to infobox component
+        )
     )); ?>
 
     <?php echo Utils\ob_load_template_part('templates/partials/building/building-intro', compact('connected_location_name')); ?>
