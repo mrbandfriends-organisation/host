83103da4906f3cfba848e07cc07e4be4db2c5aa2
David Smith <david.smith@mrbandfriends.co.uk>
2017-04-03 12:37:34 +0100

    Enable ability to open Promo CTA in new window

--- site/web/app/themes/host/templates/snippets/home/promo.php
+++ site/web/app/themes/host/templates/snippets/home/promo.php
@@ -1,6 +1,8 @@
 <?php
 use Roots\Sage\Utils;
 
+$cta_open_in_new_tab_attr = ( $cta_open_in_new_tab ) ? 'target="_blank"' : '';
+
 ?>
 
 <h2>
@@ -12,7 +14,7 @@ use Roots\Sage\Utils;
 <?php echo Utils\esc_textarea__($content);?>
 
 <?php if ( !Utils\mempty( $cta_link, $cta_text ) ): ?>	
-<a href="<?php echo esc_attr($cta_link); ?>" class="btn split-feature__btn">
+<a href="<?php echo esc_attr($cta_link); ?>" class="btn split-feature__btn" <?=$cta_open_in_new_tab_attr;?>>
 	<?php echo esc_html($cta_text); ?>
 </a>
 <?php endif ?>
