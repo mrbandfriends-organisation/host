f823cfa4fef5a89b899a45a83bbb47d178550e83
Anthony Parker <anthonyparker121@gmail.com>
2018-05-31 10:10:54 +0100

    Add options to hide sections and book now button

--- site/web/app/themes/host/templates/content-single-locations.php
+++ site/web/app/themes/host/templates/content-single-locations.php
@@ -3,13 +3,21 @@
 
     echo Utils\ob_load_template_part('templates/partials/shared/header-carousel.php');
 
-    echo Utils\ob_load_template_part('templates/partials/location/location-intro.php');
+    if ( !( get_field('hide_header', get_the_ID() ) ) ) {
+
+        echo Utils\ob_load_template_part('templates/partials/location/location-intro.php');
+
+    }
 
     echo Utils\ob_load_template_part('templates/partials/location/location-buildings.php');
 
-    echo Utils\ob_load_template_part('templates/partials/location/location-attractions.php');
+    if ( !( get_field('hide_map', get_the_ID() ) ) ) {
 
-    echo Utils\ob_load_template_part('templates/partials/shared/map');
+        echo Utils\ob_load_template_part('templates/partials/location/location-attractions.php');
+
+        echo Utils\ob_load_template_part('templates/partials/shared/map');
+
+    }
 
     if ( !( get_field('hide_news', get_the_ID() ) ) ) {
 
