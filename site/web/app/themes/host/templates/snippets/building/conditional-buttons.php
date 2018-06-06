65f9594132ea8c20093626c1308e1652c7f8e809
Anthony Parker <anthony.parker@mrbandfriends.co.uk>
2018-06-06 12:54:50 +0100

    Move button modifier to template call

--- site/web/app/themes/host/templates/snippets/building/conditional-buttons.php
+++ site/web/app/themes/host/templates/snippets/building/conditional-buttons.php
@@ -7,7 +7,7 @@
 <?php if (  $can_book && !( get_field('hide_book_now', get_the_ID()) )  ): ?>
 
     <?php $booking_url = get_field('booking_url', 'option'); ?>
-    <a href="<?= esc_attr($booking_url); ?>" class="btn btn--red <?=esc_attr($btn_modifiers);?>" <?php Extras\link_open_new_tab_attrs(); ?>>
+    <a href="<?= esc_attr($booking_url); ?>" class="btn <?=esc_attr($btn_modifiers);?>" <?php Extras\link_open_new_tab_attrs(); ?>>
         Book now
     </a>
     
