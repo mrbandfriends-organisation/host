dfdaf96ca1c7c5d284861f837ef7f4983350662e
George Putnam <george.putnam@mrbandfriends.co.uk>
2019-02-18 13:32:52 +0000

    Disabling Redirect Debug for now

---
+++ site/web/app/mu-plugins/redirect-debug.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Quiet Redirect Debug
+Description: Show all redirects
+Version: 0.1
+*/
+
+// add_filter( 'wp_redirect', function( $url ) {
+//   error_log( print_r( debug_backtrace(), true ) );
+// } );
