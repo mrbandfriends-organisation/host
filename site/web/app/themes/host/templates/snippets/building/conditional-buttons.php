ced2201e50d3ad426e0c8c7c95df5e0d98fd9edd
marcusharding <marcus.harding@mrbandfriends.co.uk>
2019-03-19 16:17:57 +0000

    pull request changes

--- site/web/app/themes/host/templates/snippets/building/conditional-buttons.php
+++ site/web/app/themes/host/templates/snippets/building/conditional-buttons.php
@@ -4,11 +4,11 @@
     $btn_modifiers = ( !empty($btn_modifiers) ) ? $btn_modifiers : '';
 ?>
 
-<?php if (  $can_book && !( get_field('hide_book_now', get_the_ID()) )  ): ?>
+<?php if (  $can_book && !( get_field('hide_book_now', get_the_ID()) ) ): ?>
 
             <?php 
-                $booking_url = get_field('book_now_url');
-                $booking_text = get_field('book_now_text');
+                $booking_url = get_field('book_now_url', get_the_ID() );
+                $booking_text = get_field('book_now_text', get_the_ID() );
             ?>
 
     <a href="<?= esc_attr($booking_url); ?>" class="btn <?=esc_attr($btn_modifiers);?>" <?php Extras\link_open_new_tab_attrs(); ?>>
